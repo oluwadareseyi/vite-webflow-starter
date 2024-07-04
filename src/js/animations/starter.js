159bea3558f3ffcadf5d69f14fb2584d96b81aa7
Nikolay Trakiyski <62264455+Ntrakiyski@users.noreply.github.com>
2024-07-04 16:25:55 +0000

    dd2

--- src/js/animations/starter.js
+++ src/js/animations/starter.js
@@ -3,7 +3,7 @@ import gsap from "gsap";
 const showBody = () => {
   gsap.to("body", { duration: 0.5, autoAlpha: 1 });
 
-  console.log("show body");
+  console.log("wasdaaaa");
 };
 
 export { showBody };
